b266a69753dc5fa1b2b57991396ea7d59b3a1218
Slava <slavaonofriuk@gmail.com>
2023-03-02 18:11:23 +0100

    praktik if_switch.js

---
+++ js/else.js
@@ -0,0 +1,31 @@
+"use strict"
+
+/* const num = '50';
+
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Много');
+} else {
+    console.log('Ok!');
+}
+
+(num === 50) ? console.log('Ok!') : console.log('Error'); // тернарный оператор */
+
+const num = '50';
+
+switch (num) {
+    case '49':
+        console.log('Неверно');
+        break;
+    case '100':
+        console.log('Неверно');
+        break;
+    case '50':
+        console.log('Правдивое');
+        break;
+    default:
+        console.log('Не в этот раз');
+        break;
+}
+
